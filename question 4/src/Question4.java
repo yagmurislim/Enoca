8d79c1cbe52e3bab4a1197890d33d77891b50e3b
islim <60823857+yagmurislim@users.noreply.github.com>
2022-05-18 14:54:01 +0300

    Add files via upload

---
+++ question 4/src/Question4.java
@@ -0,0 +1,14 @@
+public class Question4 {
+    public static void main(String args[]) {
+        int j=1;
+        for(int i=1;i<=15;i++)
+        {
+            System.out.print("*");
+            if(i==1 || i==3 || i==6 || i==10)
+            {
+                System.out.println();
+            }
+        }
+    }
+
+}
